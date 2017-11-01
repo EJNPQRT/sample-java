278e62484bb63e9e873f42075f090a389214d405
Sunil Siddala <saisunilk@gmail.com>
2017-11-01 20:21:51 +0530

    firstprogram.java

--- firstprogram.java
+++ firstprogram.java
@@ -2,7 +2,7 @@ public class firstprogram{
 public static void main(String[] args){
 for (int i=0;i<10;i++)
 {
-    System.out.println("SUNIL");
+    System.out.println("TESTING SUNIL");
 System.out.println("Hello world"+i);
 
 }
